c3adb2f39c0f589870e52cb7d2df1c17779d3e40
Tiare Figueroa <figueroa.contrerast@gmail.com>
2025-03-11 23:36:31 -0300

    primer avance

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombres :[];
+
+
+function agregarNombre() {
+    let nombreAgregado = document.getElementById("nombreAmigo").value;
+
+    if (nombreAgregado.trim() === ""){
+        alert('Por favor, ingresa un nombre válido.');
+    } else {
+
+    }
+}
